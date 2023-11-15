7e6a6e0f398514cef64964446c261c1dba3f1c3d
Focus <daniyar.danilov@gmail.com>
2023-11-15 11:29:02 +0500

    non realted comps

--- src/app/main/editNoteWindow/editNote.component.ts
+++ src/app/main/editNoteWindow/editNote.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+// import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-editNote',
@@ -7,13 +8,43 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class editNoteComponent {
   modal: boolean = true;
+  storage: any = localStorage;
+  noteName: string = '';
+  noteValue: string = '';
 
-  @Output() modalChanged: EventEmitter<boolean> = new EventEmitter<boolean>()
+  @Output() modalChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  addNote(name: any, value: any) {
+    let notes: {
+      name: string;
+      value: string;
+    }[] = JSON.parse(this.storage.getItem('notes')) || [];
+    let noteObject: {
+      name: string;
+      value: string;
+      date: string;
+    } = {
+      name: name,
+      value: value,
+      date: `${new Date().getFullYear()}.${new Date().getMonth()}.${new Date().getDate()}`,
+    };
+    notes.push(noteObject);
+    this.storage.setItem('notes', JSON.stringify(notes));
+    console.log(this.storage);
+  }
+  editNote(i: number, name: string, value: string) {
+    let notes: {
+      name: string;
+      value: string;
+    }[] = JSON.parse(this.storage.getItem('notes')) || [];
+    notes[i].name = name
+    notes[i].value = value
+    this.storage.setItem('notes', JSON.stringify(notes));
+  }
   closeModal() {
     this.modal = false;
   }
   onModalChanged() {
-    this.modalChanged.emit(this.modal)
+    this.modalChanged.emit(this.modal);
   }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,32 +7,13 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   storage: any = localStorage;
-  notesArr: any[] = JSON.parse(this.storage.getItem('notes'))
+  notesArr: any[] = JSON.parse(this.storage.getItem('notes'));
   modal: boolean | any;
-
-  addNote(name: any, value: any) {
-    let notes: { 
-      name: string; 
-      value: string;
-    }[] =
-      JSON.parse(this.storage.getItem('notes')) || [];
-    let noteObject: {
-      name: string;
-      value: string;
-      date: string
-    } = {
-      name: name,
-      value: value,
-      date: `${new Date().getFullYear()}.${new Date().getMonth()}.${new Date().getDate()}`
-    };
-    notes.push(noteObject);
-    this.storage.setItem('notes', JSON.stringify(notes));
-    console.log(this.storage);
-  }
-
   onModalChanged(value: boolean) {
-    this.modal = value
+    this.modal = value;
     console.log(this.modal);
-    
+  }
+  openModal() {
+    this.modal = true;
   }
 }
